ccf9fb8aecaf621f6873a97170851d918aed51b0
Aidanc2000 <aidan@carvalho.org.uk>
2022-10-16 13:19:51 +0100

    implemented react context for navbar in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Nav from '../components/nav';
+import { useState } from 'react';
+import { createContext } from 'react';
+export const ThemeContext = createContext('light');
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	const [theme, setTheme] = useState('light');
+	return (
+		<>
+			<ThemeContext.Provider value={theme}>
+				<Nav theme={theme} setTheme={setTheme} />
+				<Component {...pageProps} />
+			</ThemeContext.Provider>
+		</>
+	);
 }
 
 export default MyApp;
