152db35876cee7a9f4406e91857263b34d4de133
Aidanc2000 <aidan@carvalho.org.uk>
2022-10-17 15:52:45 +0100

    created new blog page which displays all blog posts

---
+++ pages/blog.tsx
@@ -0,0 +1,58 @@
+import Posts from '../components/posts';
+import matter from 'gray-matter';
+import fs, { readdirSync } from 'fs';
+import path from 'path';
+import type { NextPage, GetStaticProps } from 'next';
+type TPosts = {
+	posts: TPost[];
+};
+
+type TPost = {
+	frontmatter: { [key: string]: string };
+	slug: string;
+};
+
+const BlogPage: NextPage<TPosts> = ({ posts }) => {
+	return (
+		<section className='dark:text-white'>
+			<div className='dark:text-white'>
+				{posts.map((post) => {
+					return (
+						<>
+							<Posts post={post}></Posts>
+						</>
+					);
+				})}
+			</div>
+		</section>
+	);
+};
+
+export const getStaticProps: GetStaticProps<TPosts> = async () => {
+	const files = readdirSync(path.join('posts'));
+	// get slug and front matter from posts
+	const posts: TPost[] = files.map((filename) => {
+		const slug = filename.replace('.md', '');
+
+		// get frontmatter
+		const markdownWithMeta = fs.readFileSync(
+			path.join('posts', filename),
+			'utf-8'
+		);
+		// parses down data & renames data to frontmatter
+		const { data: frontmatter } = matter(markdownWithMeta);
+		return {
+			slug,
+			frontmatter,
+		};
+	});
+	console.log(files);
+	console.log(posts);
+	return {
+		props: {
+			posts,
+		},
+	};
+};
+
+export default BlogPage;
