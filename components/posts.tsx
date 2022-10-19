1982d451b861c2b3e13880d198ec833e5a8cd43c
Aidanc2000 <aidan@carvalho.org.uk>
2022-10-19 16:05:38 +0100

    added test image to post card component

--- components/posts.tsx
+++ components/posts.tsx
@@ -1,13 +1,19 @@
+import Image from 'next/image';
 export default function Posts({ post }) {
 	return (
 		<section className='dark:gray-900'>
 			<div className='flex justify-center gap-12 dark:gray-900'>
 				<div className='max-w-sm rounded overflow-hidden shadow-lg dark:bg-gray-600 dark:shadow-white'>
-					{/* <image className='w-full'  /> */}
 					<div className='px-6 py-4 '>
 						<h1 className='font-bold text-l mb-2 md:text-1xl lg:text-3xl'>
 							{post.frontmatter.title}
 						</h1>
+						<Image
+							src={post.frontmatter.cover_image}
+							alt=''
+							width={50}
+							height={50}
+						></Image>
 						<p className='text-gray-700 text-center dark:text-white'>
 							{post.frontmatter.description}
 						</p>
