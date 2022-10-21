9b1836186587f06c6919f25b54b2cca33b0e7608
Aidanc2000 <aidan@carvalho.org.uk>
2022-10-21 19:44:41 +0100

    wrapped app with theme div so don't need to manually do it on each component or page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import Nav from '../components/nav';
+import NavBar from '../components/NavBar';
 import { useState } from 'react';
 import { createContext } from 'react';
 export const ThemeContext = createContext('light');
@@ -9,10 +9,12 @@ function MyApp({ Component, pageProps }: AppProps) {
 	const [theme, setTheme] = useState('light');
 	return (
 		<>
-			<ThemeContext.Provider value={theme}>
-				<Nav theme={theme} setTheme={setTheme} />
-				<Component {...pageProps} />
-			</ThemeContext.Provider>
+			<div className={theme}>
+				<ThemeContext.Provider value={theme}>
+					<NavBar theme={theme} setTheme={setTheme} />
+					<Component {...pageProps} />
+				</ThemeContext.Provider>
+			</div>
 		</>
 	);
 }
