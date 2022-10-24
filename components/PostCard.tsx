f7258d90b997e40ee0e72582c7a4dbad6dd4f98c
Aidanc2000 <aidan@carvalho.org.uk>
2022-10-24 15:17:43 +0100

    destructured frontmatter - removed comments

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,21 +1,31 @@
 import Image from 'next/image';
 import { FC } from 'react';
-import Link from 'next/link';
-
 import type { TPost } from '../types';
+
 type PostCardProp = {
 	post: TPost;
 };
 
 const PostCard: FC<PostCardProp> = ({ post }) => {
-	const { frontmatter, content, slug } = post;
+	const {
+		frontmatter: {
+			title,
+			date,
+			description,
+			cover_image,
+			alternate_image,
+			tags,
+		},
+		slug,
+		content,
+	} = post;
 	return (
 		<div className=' dark:gray-900 '>
 			<a href={`/blog/${slug}`}>
 				<div className='dark:gray-900'>
 					<div className='max-w-sm rounded overflow-hidden shadow-lg dark:bg-gray-600 dark:shadow-white hover:scale-110 delay-100 hover:shadow-2xl transition-all duration-150 ease-out hover:ease-in '>
 						<Image
-							src={frontmatter.cover_image}
+							src={cover_image}
 							alt=''
 							className='w-full'
 							width={600}
@@ -23,16 +33,16 @@ const PostCard: FC<PostCardProp> = ({ post }) => {
 						/>
 						<div className='px-6 py-4 '>
 							<h1 className='font-bold text-l mb-2 md:text-1xl lg:text-3xl'>
-								{frontmatter.title}
+								{title}
 							</h1>
 
 							<p className='text-gray-700 text-center dark:text-white'>
-								{frontmatter.description}
+								{description}
 							</p>
 						</div>
 						<div className='px-6 pt-4 pb-2'>
 							<span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
-								{frontmatter.date}
+								{date}
 							</span>
 						</div>
 					</div>
