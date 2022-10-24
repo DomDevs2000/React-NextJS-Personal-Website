eb2c6f6cad8b749e1ab72ff0505435eb6e620f3d
Aidanc2000 <aidan@carvalho.org.uk>
2022-10-24 12:29:36 +0100

    added types folder

---
+++ types/index.tsx
@@ -0,0 +1,25 @@
+export type TPost = {
+	frontmatter: TFrontmatter;
+	slug: string;
+	content: string;
+};
+
+// export type TPosts = {
+// 	posts: TPost[];
+
+// };
+
+export type TProject = {
+	frontmatter: TFrontmatter;
+	slug: string;
+	content: string;
+};
+
+export type TFrontmatter = {
+	tags: string[];
+	date: string;
+	title: string;
+	description: string;
+	cover_image: string;
+	alternate_image: string;
+};
