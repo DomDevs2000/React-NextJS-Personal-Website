088e59d56ee34b76e0bc6c6804f98a5af4680b8a
Aidanc2000 <aidan@carvalho.org.uk>
2022-11-09 14:45:33 +0000

    fixed slug replacing .md

--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -68,7 +68,7 @@ export async function getStaticPaths() {
     const files = fs.readdirSync(path.join('projects'));
     const paths = files.map((filename) => ({
         params: {
-            slug: filename.replace('md', '')
+            slug: filename.replace('.md', '')
         }
     }));
 
