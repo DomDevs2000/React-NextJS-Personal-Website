f0201c715a0bdd7e9efb8d0c66d0bd7068ca3125
Aidanc2000 <aidan@carvalho.org.uk>
2022-11-09 15:07:15 +0000

    updated prose headings/a elements to be viewable in dark mode

--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -53,7 +53,7 @@ const ProjectPage: FC<ProjectPageProp> = ({ project }) => {
 
                 <div className="text-sm md:text-lg lg:text-xl   flex justify-center  px-3 py-1  text-gray-700 mr-2 mb-2">
                     <ReactMarkdown
-                        className="prose dark:text-white dark:prose-a:text-gray-400 dark:prose-headings:text-gray-100- dark:prose-code:text-gray-400"
+                        className="prose dark:text-white dark:prose-a:text-white dark:prose-headings:text-white dark:prose-headings:text-gray-100- dark:prose-code:text-gray-400"
                         remarkPlugins={[remarkGfm]}
                     >
                         {content}
