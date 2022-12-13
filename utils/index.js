84be0919fd76d71b1ce8e8c20d9633b11202290d
Aidanc2000 <aidan@carvalho.org.uk>
2022-12-13 00:55:01 +0000

    converted file to typescript - fixed type errors in function

--- utils/index.js
+++
@@ -1,8 +0,0 @@
-export const sortByDate = (a, b) => {
-	return new Date(b.frontmatter.date) - new Date(a.frontmatter.date);
-};
-
-//ISO DATE FORMAT https://en.wikipedia.org/wiki/ISO_8601
-
-//  new Date('2022-10-19T16:28:42+00:00') .getFullYear()
-//format to front end in posts.tsx
---
+++ utils/index.ts
@@ -0,0 +1,4 @@
+
+export const sortByDate = (a: any ,b: any) => {
+	return new Date(b.frontmatter.date).valueOf() - new Date(a.frontmatter.date).valueOf();
+};
