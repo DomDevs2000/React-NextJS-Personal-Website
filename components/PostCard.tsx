5322cd3f7d9de5bc999cfecbcd098122973d8168
Aidanc2000 <aidan@carvalho.org.uk>
2022-12-16 17:59:39 +0000

    updated card css

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -21,36 +21,34 @@ export const PostCard: FC<PostCardProp> = ({ post }) => {
         content
     } = post;
     return (
-        <div className="dark:gray-900">
-            <div className="max-h-fit max-w-fit rounded overflow-hidden shadow-lg dark:bg-slate-500 dark:shadow-slate-600 hover:scale-105 delay-100 hover:shadow-2xl transition-all duration-150 ease-out hover:ease-in ">
-                <Link href={`/blog/${slug}`}>
+        <div className="dark:gray-900 cursor-pointer">
+            <Link href={`/blog/${slug}`}>
+                <div className="max-h-fit max-w-fit rounded overflow-hidden shadow-lg dark:bg-slate-500 dark:shadow-slate-600 hover:scale-105 delay-100 hover:shadow-2xl transition-all duration-150 ease-out hover:ease-in ">
                     <Image
                         src={cover_image}
                         alt=""
-                        className="w-full cursor-pointer"
+                        className="w-full"
                         width={700}
                         height={350}
                     />
-                </Link>
 
-                <div className="px-4 py-2 ">
-                    <Link href={`/blog/${slug}`}>
-                        <h1 className="font-bold text-center hover:underline lg:text-2xl mb-2 cursor-pointer">
+                    <div className="px-4 py-2 ">
+                        <h1 className="font-bold text-center hover:underline lg:text-2xl mb-2">
                             {title}
                         </h1>
-                    </Link>
-                    <Link href={`/blog/${slug}`}>
-                    <p className="text-gray-700 text-sm cursor-pointer dark:text-white">
-                        {description}
-                    </p>
-                    </Link>
+
+                        <p className="text-gray-700 text-sm dark:text-white">
+                            {description}
+                        </p>
+                    </div>
+
+                    <div className="px-4 pt-2 pb-2 mb-2">
+                        <span className="inline-block bg-gray-200 dark:bg-gray-400 rounded-full px-3 py-1 text-sm dark:text-black text-gray-700 mr-2 mb-2">
+                            {read_length} read | {date}
+                        </span>
+                    </div>
                 </div>
-                <div className="px-4 pt-2 pb-2 mb-2 ">
-                    <span className="inline-block  bg-gray-200 dark:bg-gray-400 rounded-full px-3 py-1 text-sm dark:text-black text-gray-700 mr-2 mb-2">
-                        {read_length} read | {date}
-                    </span>
-                </div>
-            </div>
+            </Link>
         </div>
     );
 };
--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -18,7 +18,6 @@ export const ProjectCard: FC<ProjectCardProp> = ({ project }) => {
         return (
             <li
                 key={tag}
-                // style={{font-variant: small-caps}}
                 className="px-5 py-5 inline-block dark:bg-gray-400 bg-gray-200 rounded-full px-3 py-1 text-xs dark:text-black text-gray-700 mr-2 mb-2"
             >
                 <Link href={`/tags/${tag}`}>{tag}</Link>
@@ -26,35 +25,30 @@ export const ProjectCard: FC<ProjectCardProp> = ({ project }) => {
         );
     });
     return (
-        <div className="dark:gray-900 ">
-            <div className="max-h-fit rounded overflow-hidden shadow-lg dark:bg-slate-500 dark:shadow-slate-600 hover:scale-105 delay-100 hover:shadow-2xl transition-all duration-150 ease-out hover:ease-in ">
-                <Link href={`/projects/${slug}`}>
+        <div className="dark:gray-900 cursor-pointer">
+            <Link href={`/projects/${slug}`}>
+                <div className="max-h-fit rounded overflow-hidden shadow-lg dark:bg-slate-500 dark:shadow-slate-600 hover:scale-105 delay-100 hover:shadow-2xl transition-all duration-150 ease-out hover:ease-in ">
                     <Image
                         src={cover_image}
                         alt=""
-                        className="w-full cursor-pointer"
+                        className="w-full"
                         width={650}
                         height={350}
                     />
-                </Link>
-
-                <div className="px-4 py-2">
-                    <Link href={`/projects/${slug}`}>
-                        <h1 className="font-bold text-center lg:text-2xl mb-2 hover:underline cursor-pointer">
+                    <div className="px-4 py-2">
+                        <h1 className="font-bold text-center lg:text-2xl mb-2 hover:underline">
                             {title}
                         </h1>
-                    </Link>
-                    <Link href={`/projects/${slug}`}>
-                    <p className="text-gray-700 text-sm dark:text-white cursor-pointer">
-                        {description}
-                    </p>
-                    </Link>
-                </div>
+                        <p className="text-gray-700 text-sm dark:text-white">
+                            {description}
+                        </p>
+                    </div>
 
-                <div className="grid px-4 pt-3  pb-2">
-                    <ul> {renderTags}</ul>
+                    <div className="grid px-4 pt-3 pb-2">
+                        <ul> {renderTags}</ul>
+                    </div>
                 </div>
-            </div>
+            </Link>
         </div>
     );
 };
