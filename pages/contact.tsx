3df12bccbb0ca59191043b82698592849e30dab3
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2022-12-21 15:47:28 +0000

    updated h1

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -4,7 +4,7 @@ import { Contact } from '../components/contact';
 const contactPage = () => {
     return (
         <section className='min-h-screen dark:bg-gray-900 dark:text-white font-semibold text-center '>
-            <h1 className='m-15'>Fill In The Form Below And I'll Be In Touch Soon</h1>
+            <h1 className='m-15'>Fill In The Form Below And I&apos;ll Be In Touch Soon</h1>
             <Contact />
         </section>
     );
