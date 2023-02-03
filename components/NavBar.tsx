b240278153ffe330d1f67c6461ad2bab5e081b79
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2023-02-03 15:36:43 +0000

    updated navbar css

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -12,16 +12,17 @@ const Nav: FC<NavProp> = ({ theme, setTheme }) => {
 
     return (
         <div className={theme}>
-            <section className="max-h-600  dark:bg-black  px-10 font-sans">
+            <nav className="bg-blue-700">
                 <div className="flex items-center justify-between border-b border-gray-400 py-8">
-                    <h1 className="uppercase dark:text-white hover:underline hover:scale-105">
-                        <Link href="/">
-                            <Link href="/">Dom Devs</Link>
-                        </Link>
-                    </h1>
+                    <Link href="/">
+                        <div className="inline-flex  px-4 p-2 text-xl font-bold uppercase tracking-wider text-white hover:cursor-pointer hover:scale-110 hover:bg-blue-800">
+                            Dom Devs
+                        </div>
+                    </Link>
+
                     <button className="ml-8 px-4 py-2 theme-toggle">
                         <BsFillMoonStarsFill
-                            color="gray"
+                            color="white"
                             onClick={() => {
                                 const newTheme =
                                     theme === 'light' ? 'dark' : 'light';
@@ -32,14 +33,15 @@ const Nav: FC<NavProp> = ({ theme, setTheme }) => {
                         />
                     </button>
                     <nav>
+                        {/*mobile menu code*/}
                         <section className="MOBILE-MENU flex lg:hidden">
                             <button
-                                className="HAMBURGER-ICON space-y-2"
+                                className="HAMBURGER-ICON space-y-2 ml-auto"
                                 onClick={() => setIsNavOpen((prev) => !prev)}
                             >
-                                <span className="block h-0.5 w-8 animate-pulse dark:bg-white bg-gray-600"></span>
-                                <span className="block h-0.5 w-8 animate-pulse dark:bg-white bg-gray-600"></span>
-                                <span className="block h-0.5 w-8 animate-pulse dark:bg-white bg-gray-600"></span>
+                                <span className="block h-0.5 w-8 animate-pulse  bg-white"></span>
+                                <span className="block h-0.5 w-8 animate-pulse  bg-white"></span>
+                                <span className="block h-0.5 w-8 animate-pulse  bg-white"></span>
                             </button>
                             {isNavOpen && (
                                 <div
@@ -50,11 +52,11 @@ const Nav: FC<NavProp> = ({ theme, setTheme }) => {
                                     }
                                 >
                                     <button
-                                        className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
-                                        onClick={() => setIsNavOpen(false)} // change isNavOpen state to false to close the menu
+                                        className="CROSS-ICON text-white absolute top-0 right-0 px-8 py-8"
+                                        onClick={() => setIsNavOpen(false)}
                                     >
                                         <svg
-                                            className="h-8 w-8 text-gray-600"
+                                            className="h-8 w-8 text-white"
                                             viewBox="0 0 24 24"
                                             fill="none"
                                             stroke="currentColor"
@@ -76,8 +78,8 @@ const Nav: FC<NavProp> = ({ theme, setTheme }) => {
                                             />
                                         </svg>
                                     </button>
-                                    <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px] dark:text-white">
-                                        <li className="border-b border-gray-400 my-8 uppercase">
+                                    <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px]">
+                                        <li className="flex px-4 py-2 font-medium text-white  rounded-md hover:scale-110 hover:bg-blue-800">
                                             <button
                                                 onClick={() =>
                                                     setIsNavOpen(false)
@@ -88,7 +90,7 @@ const Nav: FC<NavProp> = ({ theme, setTheme }) => {
                                                 </Link>
                                             </button>
                                         </li>
-                                        <li className="border-b border-gray-400 my-8 uppercase ">
+                                        <li className="flex px-4 py-2 font-medium text-white  rounded-md  hover:scale-110 hover:bg-blue-800">
                                             <button
                                                 onClick={() =>
                                                     setIsNavOpen(false)
@@ -97,7 +99,7 @@ const Nav: FC<NavProp> = ({ theme, setTheme }) => {
                                                 <Link href="/blog">Blog</Link>
                                             </button>
                                         </li>
-                                        <li className="border-b border-gray-400 my-8 uppercase">
+                                        <li className="flex px-4 py-2 font-medium text-white rounded-md hover:scale-110  hover:bg-blue-800">
                                             <button
                                                 onClick={() =>
                                                     setIsNavOpen(false)
@@ -113,17 +115,20 @@ const Nav: FC<NavProp> = ({ theme, setTheme }) => {
                             )}
                         </section>
 
-                        <ul className="DESKTOP-MENU hidden space-x-8 lg:flex dark:text-white">
-                            <li className="hover:scale-125 delay-100 ">
-                                <Link href="/projects">Projects</Link>
-                            </li>
-                            <li className="hover:scale-125 delay-100 ">
-                                <Link href="/blog">Blog</Link>
-                            </li>
-                            <li className="hover:scale-125 delay-100 ">
-                                <Link href="/contact">Contact</Link>
-                            </li>
-                        </ul>
+                        <div className="MENU-LINK-MOBILE-OPEN  w-full lg:inline-flex lg:w-auto mt-2 lg:mt-0">
+                            <ul className="flex lg:flex-row flex-col space-y-y lg:space-y-0 lg:space-x-2">
+                                <li className="flex px-4 py-2 font-medium text-white  rounded-md  hover:scale-110  hover:bg-blue-800">
+                                    <Link href="/projects">Projects</Link>
+                                </li>
+                                <li className="flex px-4 py-2 font-medium text-white  rounded-md  hover:scale-110  hover:bg-blue-800">
+                                    <Link href="/blog">Blog</Link>
+                                </li>
+
+                                <li className="flex px-4 py-2 font-medium text-white rounded-md  hover:scale-110  hover:bg-blue-800">
+                                    <Link href="/contact">Contact Me</Link>
+                                </li>
+                            </ul>
+                        </div>
                     </nav>
                     <style>{`
       .hideMenuNav {
@@ -145,16 +150,141 @@ const Nav: FC<NavProp> = ({ theme, setTheme }) => {
         
         
       }
-
       .dark .showMenuNav{
-        background-color: rgb(19,24,39)
+        background-color: rgb(29,78,216)
       }
-
     `}</style>
                 </div>
-            </section>
+            </nav>
         </div>
     );
 };
+{
+    /*<nav className="bg-blue-700" >*/
+}
+{
+    /*    <div className="container flex flex-wrap py-2 mx-auto">*/
+}
+{
+    /*        <Link href="/">*/
+}
+{
+    /*            <div className="inline-flex  px-4 p-2 text-xl font-bold uppercase tracking-wider text-white">*/
+}
+{
+    /*                Dom Devs*/
+}
+{
+    /*            </div>*/
+}
+{
+    /*        </Link>*/
+}
+
+{
+    /*        <button className=" CROSS-ICON lg:hidden inline-flex items-center justify-center  text-white border h-10 w-10 rounded-md outline-none focus:outline-none ml-auto"*/
+}
+
+{
+    /*                onClick={() => setIsNavOpen((prev) => !prev)}*/
+}
+
+{
+    /*                >*/
+}
+{
+    /*            <svg*/
+}
+{
+    /*                xmlns="http://www.w3.org/2000/svg"*/
+}
+{
+    /*                fill="none"*/
+}
+{
+    /*                viewBox="0 0 24 24"*/
+}
+{
+    /*                strokeWidth={1.5}*/
+}
+{
+    /*                stroke="currentColor"*/
+}
+{
+    /*                className="w-6 h-6"*/
+}
+{
+    /*            >*/
+}
+{
+    /*                <path*/
+}
+{
+    /*                    strokeLinecap="round"*/
+}
+{
+    /*                    strokeLinejoin="round"*/
+}
+{
+    /*                    d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"*/
+}
+{
+    /*                />*/
+}
+{
+    /*            </svg>*/
+}
+{
+    /*        </button>*/
+}
+{
+    /*    </div>*/
+}
+{
+    /*    <div className="MENU-LINK-MOBILE-OPEN w-full lg:inline-flex lg:w-auto mt-2 lg:mt-0">*/
+}
+{
+    /*        <ul className="flex lg:flex-row flex-col space-y-y lg:space-y-0 lg:space-x-2">*/
+}
+{
+    /*            <li className="flex px-4 py-2 font-medium text-white  rounded-md hover:bg-blue-800">*/
+}
+{
+    /*                <Link href="/projects">Projects</Link>*/
+}
+{
+    /*            </li>*/
+}
+{
+    /*            <li className="flex px-4 py-2 font-medium text-white  rounded-md hover:bg-blue-800">*/
+}
+{
+    /*                <Link href="/blog">Blog</Link>*/
+}
+{
+    /*            </li>*/
+}
+{
+    /*            <li className="flex px-4 py-2 font-medium text-white rounded-md hover:bg-blue-800">*/
+}
+{
+    /*                <Link href="/contact">Contact Me</Link>*/
+}
+{
+    /*            </li>*/
+}
+{
+    /*        </ul>*/
+}
+{
+    /*    </div>*/
+}
+{
+    /*</nav>*/
+}
+
+//
+//     );
+// };
 
 export default Nav;
