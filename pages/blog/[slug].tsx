ac310e8c6159cdc512b824f58c1400126c7eea11
Aidan Dominic Carvalho <109763238+DomDevs2000@users.noreply.github.com>
2023-02-07 23:58:43 +0000

    fixed css in blog page

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -56,7 +56,7 @@ const PostPage: FC<PostPageProp> = ({ post }) => {
                         height={400}
                     ></Image>
                 </div>
-                <div className="text-sm md:text-lg lg:text-xl flex justify-center px-3 py-1 text-gray-700 mr-2 mb-2">
+                <div className="text-sm md:text-lg lg:text-xl  flex justify-center  px-3 py-1  text-gray-700 mr-2 mb-2">
                     <ReactMarkdown
                         className="prose dark:text-white dark:prose-a:text-gray-400 dark:prose-headings:text-white dark:prose-strong:text-white"
                         remarkPlugins={[remarkGfm]}
